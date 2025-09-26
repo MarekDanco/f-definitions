b0d0494f3b69f7a007de97eb607a57a2f2c9092d
marek <marekdanco.mail@gmail.com>
2025-09-26 19:12:46 +0200

    single invocation test

---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""Function synthesis project"""
+
+import sys
+import argparse
+import z3
+from formula import Formula
+
+
+def parse_command_line() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Tool for function synthesis")
+    parser.add_argument(
+        "filename",
+        help="Filename with input formula",
+        default="-",
+        nargs="?",
+        type=str,
+    )
+    parser.add_argument(
+        "-v", "--verbose", help="Show all messages", action="store_true"
+    )
+    args = parser.parse_args()
+    return args
+
+
+def display_formula(formula, message=None) -> None:
+    if message is not None:
+        print(f"{message} {formula}", flush=True)
+    else:
+        print(formula, flush=True)
+
+
+def display_constraints(constraints, message=None) -> None:
+    for constraint in constraints:
+        display_formula(constraint, message)
+
+
+def parse_input_file(file_path: str):
+    s = z3.Solver()
+    input_path = "examples/test.smt2" if file_path == "-" else file_path
+    s.from_file(input_path)
+    constraints = s.assertions()
+    return constraints
+
+
+def main():
+    args = parse_command_line()
+    constraints = parse_input_file(args.filename)
+    for cons in constraints:
+        f = Formula(cons)
+        display_formula(f.formula)
+        symbs = f.symbols
+        for s in symbs:
+            print(s, f"is single invocation? {f.is_single_invocation(s)}")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
