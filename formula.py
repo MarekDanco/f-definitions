b0379bb3ecd4d8564e234f9208fa4f55d30f5b3b
marek <marekdanco.mail@gmail.com>
2025-09-29 21:45:54 +0200

    bug found

--- formula.py
+++ formula.py
@@ -8,6 +8,7 @@ class Formula:
         self.symbol_expr = {}
         self.collect_symbs_cache = {}
         self.symbols = self.collect_symbs(formula, [])
+        print(self.symbol_args)
 
     def _translate_var_id(self, var, scope):
         var_id = z3.get_var_index(var)
@@ -60,6 +61,8 @@ class Formula:
         else:
             new_scope = scope
         if expr_id in self.collect_symbs_cache:
+            # de Bruijn variables bug
+            print(expr)
             return self.collect_symbs_cache[expr_id]
         symbs = set()
         if z3.is_app(expr) and expr.decl().kind() == z3.Z3_OP_UNINTERPRETED:
@@ -90,10 +93,31 @@ class Formula:
             if result is not None:
                 return result
 
-    def child_is_scope(self, child, scope):
-        if child.get_id() == scope.get_id():
-            return True
-        return False
+    def get_deskolem_var(self, symbol, scope):
+        args = list(self.symbol_args[symbol][scope])[0]
+        var_name = f"deskolem_{symbol.name()}_{hash(args) % 10000}"
+        return z3.Const(var_name, symbol.range())
+
+    # TODO check the universally quantified variables used in arguments
+    # TODO place an existential variable after the last such universal variable
+    # TODO substitute
+
+    def deskolemize(self, symbol, scope):
+        expr = list(self.symbol_expr[symbol][scope])[0]
+        print(scope.body())
+        print(expr)
+        deskolem_var = self.get_deskolem_var(symbol, scope)
+        sub = [(expr, deskolem_var)]
+        f_deskolem = z3.substitute(scope.body(), sub)
+        vs = [
+            z3.Const(scope.var_name(i), scope.var_sort(i))
+            for i in range(scope.num_vars())
+        ]
+        new_scope = z3.ForAll(
+            vs,
+            z3.Exists([deskolem_var], f_deskolem),
+        )
+        return new_scope
 
     def test_deskolemize(self, formula):
         for scope in formula.children():
@@ -104,45 +128,5 @@ class Formula:
                 res = self.is_single_invocation(symbol, formula)
                 print(f"{symbol} is single invocation? {res}")
                 if res:
-                    args = list(self.symbol_args[symbol][formula])[0]
-                    print(f"arguments {args}")
-                    var_name = f"deskolem_{symbol.name()}_{hash(args) % 10000}"
-                    deskolem_var = z3.Const(var_name, symbol.range())
-                    expr = list(self.symbol_expr[symbol][formula])[0]
-                    f_deskolem = z3.substitute(
-                        formula.body(),
-                        [
-                            (
-                                expr,
-                                deskolem_var,
-                            )
-                        ],
-                    )
-                    print("deskolem", f_deskolem)
-                    vs = [
-                        z3.Const(formula.var_name(i), formula.var_sort(i))
-                        for i in range(formula.num_vars())
-                    ]
-                    new_f = z3.ForAll(
-                        vs,
-                        z3.Exists([deskolem_var], f_deskolem),
-                    )
-                    print("new f", new_f)
-                    new_f_body = new_f.body()
-                    print("new f body", new_f_body)
-                    print("is new f body quant?", z3.is_quantifier(new_f_body))
-                    print(
-                        "new f body vars",
-                        [new_f_body.var_name(i) for i in range(new_f_body.num_vars())],
-                    )
+                    print(f"new scope {self.deskolemize(symbol, formula)}")
             print()
-
-    def deskolemize(self, scope, symbol):
-        """Deskolemize a single invocation function symbol."""
-        assert self.is_single_invocation(
-            symbol, scope
-        ), f"{symbol!r} is not single invocation within the scope of {scope!r}"
-        assert z3.is_quantifier(scope)
-        # TODO check the universally quantified variables used in arguments
-        # TODO place an existential variable after the last such universal variable
-        # TODO substitute
