c10b1c0f77d0864ae06dbd37deb29fbc4f56f3fc
marek <marekdanco.mail@gmail.com>
2025-12-14 20:26:17 +0100

    handling conjunction

--- reqpivot.py
+++ reqpivot.py
@@ -2,100 +2,40 @@
 """Module for testing ReqPivot condition"""
 
 import argparse
-from functools import cache
 from typing import Literal
 
 import z3
 
-from utils import (
-    contains_const_func,
-    has_quantifiers,
-    is_func,
-    is_ground,
-    is_offset_term,
-    offset_vars_ground,
-)
-
-
-@cache
-def _get_entities_recursive(expr):
-    """Recursively extract entities from a Z3 expression."""
-    if is_ground(expr):
-        return frozenset()
-    if is_func(expr):
-        assert expr.decl().arity() == 1, f"Function {expr} has arity greater than 1"
-        assert is_offset_term(
-            expr.arg(0)
-        ), f"The argument of {expr} is not an offset term"
-        return frozenset({expr})
-
-    res = set()
-    for child in expr.children():
-        res |= _get_entities_recursive(child)
-    return frozenset(res)
+from conjunct import ConjunctionsManager
+from utils import get_entities, has_quantifiers, simplify, split_entities
 
 
 class ReqPivot:
-    def __init__(self, formula):
-        self.formula = formula
-        self.entities = []
-        self.model: None | z3.ModelRef = None
-
-    def _normalize(self, direction: Literal["up", "down"]):
-        # TODO deal with conjunction and disjunction
-        if z3.is_and(self.formula.body()) or z3.is_or(self.formula.body()):
-            print("WARNING: testing without normalizing")
-        return self.entities
-
-    def _get_offset(self, entity) -> int:
-        assert is_offset_term(entity.arg(0)), f"Expression {entity} is not an entity"
-        entity = z3.simplify(entity, som=True)
-        _, ground = offset_vars_ground(list(entity.arg(0).children()))
-
-        if self.model is not None:
-            result = self.model.eval(sum(ground), model_completion=True)
-        else:
-            for g in ground:
-                if contains_const_func(g):
-                    assert False, "Not implemented"
-            result = sum(g.as_long() for g in ground)
-
-        if isinstance(result, int):
-            return result
-
-        assert z3.is_int_value(result), f"{entity}, {ground}, {result}, {type(result)}"
-        return result.as_long()  # type: ignore
-
-    def _is_target(self, e, target, direction: Literal["up", "down"]):
-        """Compare entity e to the current target"""
-        e_offset = self._get_offset(e)
-        target_offset = self._get_offset(target)
-        if direction == "down":
-            return e_offset < target_offset
-        return e_offset > target_offset
-
-    def _split_entities(self, entities, direction: Literal["up", "down"]):
-        target = entities[0]
-        for e in entities[1:]:
-            if self._is_target(e, target, direction):
-                target = e
-        existential = [e for e in entities if e is not target]
-        return existential, [target]
+    def __init__(self):
+        self.model: None | z3.ModelRef = None  # ground model
 
     def _get_subs(self, entities):
+        """Get entity substitutions"""
         constants = [z3.FreshConst(e.sort()) for e in entities]
         return constants, [(e, c) for e, c in zip(entities, constants)]
 
-    def test(self, direction: Literal["up", "down"]):
-        self.entities = list(_get_entities_recursive(self.formula))
+    def _process(self, assertion, direction: Literal["up", "down"]):
+        assert z3.is_quantifier(assertion), f"{assertion} is not quantified"
+        matrix = assertion.body()
+        if not z3.is_and(matrix):
+            return simplify(matrix), list(get_entities(matrix))
+        conj_manager = ConjunctionsManager(matrix, direction)
+        return conj_manager.manage()
+
+    def test(self, assertion, direction: Literal["up", "down"]):
+        formula, self.entities = self._process(assertion, direction)
         assert self.entities, "Found no entities"
-        norm_entities = self._normalize(direction)
-        existential, target = self._split_entities(norm_entities, direction)
+        existential, target = split_entities(self.entities, direction)
 
         existential_consts, existential_subs = self._get_subs(existential)
         target_const, target_sub = self._get_subs(target)
 
-        test_body = z3.substitute(self.formula.body(), existential_subs + target_sub)
+        test_body = z3.substitute(formula, existential_subs + target_sub)
         test_formula = z3.ForAll(existential_consts, z3.Exists(target_const, test_body))
 
         lia_solver = z3.SolverFor("LIA")
@@ -122,9 +62,9 @@ def main():
     assertions = [a for a in s.assertions() if has_quantifiers(a)]  # type: ignore
     for a in assertions:
         print(a)
-        rp = ReqPivot(a)
-        print("up", rp.test("up"))
-        print("down", rp.test("down"))
+        rp = ReqPivot()
+        print("up", rp.test(a, "up"))
+        print("down", rp.test(a, "down"))
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,6 +1,72 @@
+"""Utility functions"""
+
 from functools import cache
+from typing import Literal
 
 import z3
+
+
+def simplify(expr):
+    return z3.simplify(expr, som=True)
+
+
+@cache
+def get_entities(expr):
+    """Recursively extract entities from a Z3 expression."""
+    if is_ground(expr):
+        return frozenset()
+    if is_func(expr):
+        assert expr.decl().arity() == 1, f"Function {expr} has arity greater than 1"
+        assert is_offset_term(
+            expr.arg(0)
+        ), f"The argument of {expr} is not an offset term"
+        return frozenset({simplify(expr)})
+
+    res = set()
+    for child in expr.children():
+        res |= get_entities(child)
+    return frozenset(res)
+
+
+def get_offset(entity, model: None | z3.ModelRef = None) -> int:
+    assert is_offset_term(entity.arg(0)), f"Expression {entity} is not an entity"
+    entity = simplify(entity)
+    _, ground = offset_vars_ground(list(entity.arg(0).children()))
+
+    if model is not None:
+        result = model.eval(sum(ground), model_completion=True)
+    else:
+        if any(contains_const_func(g) for g in ground):
+            assert False, "Not implemented"
+        result = sum(g.as_long() for g in ground)
+
+    if isinstance(result, int):
+        return result
+
+    assert z3.is_int_value(result), (
+        "entity, ground, result, result type:",
+        f"{entity}, {ground}, {result}, {type(result)}",
+        "result type expected to be integer",
+    )
+    return result.as_long()  # type: ignore
+
+
+def is_target(e, target, direction: Literal["up", "down"]):
+    """Compare entity e to the current target"""
+    e_offset = get_offset(e)
+    target_offset = get_offset(target)
+    if direction == "down":
+        return e_offset < target_offset
+    return e_offset > target_offset
+
+
+def split_entities(entities, direction: Literal["up", "down"]):
+    target = entities[0]
+    for e in entities[1:]:
+        if is_target(e, target, direction):
+            target = e
+    existential = [e for e in entities if e is not target]
+    return existential, [target]
 
 
 @cache
@@ -10,7 +76,6 @@ def has_quantifiers(expr):
     return any(has_quantifiers(c) for c in expr.children())
 
 
-@cache
 def is_func(expr):
     """Check if expr is uninterpreted function symbol with arity > 0"""
     return (
@@ -20,7 +85,6 @@ def is_func(expr):
     )
 
 
-@cache
 def is_const(expr):
     """Check if expr is uninterpreted function symbol with arity 0"""
     return (
@@ -30,19 +94,16 @@ def is_const(expr):
     )
 
 
-@cache
 def contains_const_func(expr):
     if is_const(expr) or is_func(expr):
         return True
     return any(contains_const_func(c) for c in expr.children())
 
 
-@cache
 def is_arithmetic_op(expr):
     return z3.is_add(expr) or z3.is_sub(expr)
 
 
-@cache
 def is_ground(expr):
     if z3.is_var(expr):
         return False
@@ -61,8 +122,8 @@ def offset_vars_ground(children):
     return vs, ground
 
 
-@cache
 def is_offset_term(expr):
+    expr = simplify(expr)
     if z3.is_var(expr):
         return True
     if is_ground(expr):
