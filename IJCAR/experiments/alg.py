d4c36c8378d84ca235fcb8efab50920a98e31c1c
Stefan Ratschan <stefan.ratschan@cs.cas.cz>
2026-01-21 17:48:15 +0100

    example definition in separate function

--- IJCAR/experiments/alg.py
+++ IJCAR/experiments/alg.py
@@ -3,16 +3,21 @@ from z3 import *
 solver = z3.SolverFor("UFLIA")
 solver.set(mbqi=True)
 
-pf= [Bool('pf1'), Bool('pf2')]          # is a pivot
-f= Function('f', IntSort(), IntSort())
-offsets= [1, 0]
-x= Int('x')
-occf1= Int('occf1')
-occf2= Int('occf2')
-argf= [0]
-F= f(argf[0])==0
-Q= Or(x<0, x>=1000, f(x+offsets[0])==f(x+offsets[1])+1)
-Qp= Or(x<0, x>=1000, occf1==occf2+1)
+def def_example():
+    global pf, f, offsets, x, occf, argf, F, Q, Qp
+    
+    pf= [Bool('pf1'), Bool('pf2')]          # is a pivot
+    f= Function('f', IntSort(), IntSort())
+    offsets= [1, 0]
+    x= Int('x')
+    occf= [Int('occf1'), Int('occf2')]      # variables for occurrences of f
+    argf= [0]
+    F= f(argf[0])==0
+    Q= Or(x<0, x>=1000, f(x+offsets[0])==f(x+offsets[1])+1)
+    Qp= Or(x<0, x>=1000, occf[0]==occf[1]+1)
+
+
+def_example()    
 bmax= 0
 res="UNSAT"
 solver.add(F, substitute(Q, (x, IntVal(0))))
@@ -21,10 +26,10 @@ while(solver.check()!=unsat):
     solver.add(Or(Not(pf[0]), Not(pf[1]), offsets[0]==offsets[1]))
     solver.add(Or(Not(pf[0]), pf[1], offsets[0]>offsets[1]))
     solver.add(Or(Not(pf[1]), pf[0], offsets[1]>offsets[0]))
-    solver.add(Or(pf[0], pf[1], ForAll(x, ForAll(occf1, ForAll(occf2, Qp)))))
-    solver.add(Or(pf[0], Not(pf[1]), ForAll(x, ForAll(occf2, Exists(occf1, Qp)))))
-    solver.add(Or(Not(pf[0]), pf[1], ForAll(x, ForAll(occf1, Exists(occf2, Qp)))))
-    solver.add(Or(Not(pf[0]), Not(pf[1]), Exists(x, Exists(occf2, ForAll(occf1, Qp)))))
+    solver.add(Or(pf[0], pf[1], ForAll(x, ForAll(occf[0], ForAll(occf[1], Qp)))))
+    solver.add(Or(pf[0], Not(pf[1]), ForAll(x, ForAll(occf[1], Exists(occf[0], Qp)))))
+    solver.add(Or(Not(pf[0]), pf[1], ForAll(x, ForAll(occf[0], Exists(occf[1], Qp)))))
+    solver.add(Or(Not(pf[0]), Not(pf[1]), Exists(x, Exists(occf[1], ForAll(occf[0], Qp)))))
     solver.add(Or(Not(pf[0]), And([argf[arg]<=bmax+offsets[0] for arg in range(0, len(argf))])))
     solver.add(Or(Not(pf[1]), And([argf[arg]<=bmax+offsets[1] for arg in range(0, len(argf))])))
     subs=[ (x, IntVal(i)) for i in range(0,bmax+1)]
