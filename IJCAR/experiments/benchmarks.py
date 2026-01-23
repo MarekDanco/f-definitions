1837b21f69b1353178c75dc531265ac3b4945753
stefan-nugraha <nugraang@fit.cvut.cz>
2026-01-23 15:33:03 +0100

    Added another class with 2 functions

--- IJCAR/experiments/benchmarks.py
+++ IJCAR/experiments/benchmarks.py
@@ -11,6 +11,18 @@ class Incr:
     Q= Or(x<0, x>=1000, f(x+offsets[0][0])==f(x+offsets[0][1])+1)
     Qp= Or(x<0, x>=1000, occ[0][0]==occ[0][1]+1)
 
+class Incr2Functions:
+    x= Int('x')                                 # the quantified variable    
+    f= Function('f', IntSort(), IntSort())
+    g= Function('g', IntSort(), IntSort())
+    offsets= [[1, 0],[0]]
+
+    occ= [[Int('occf1'), Int('occf2')],[Int('occg1')]]     # variables for occurrences of funct. symb. (u, v in the paper)
+    
+    argF= [[0],[]]                             # arguments of uninterpreted function symbols in F
+    F= f(argF[0][0])==0
+    Q= Or(x<0, x>=1000, f(x+offsets[0][0])==f(x+offsets[0][1])+g(x+offsets[1][0]))
+    Qp= Or(x<0, x>=1000, occ[0][0]==occ[0][1]+occ[1][0])
 
 class Test:
     x= Int('x')                                 # the quantified variable    
