07d96c2bda64e3646ec199e92de89ae6c3d98d9e
Stefan Ratschan <stefan.ratschan@cs.cas.cz>
2026-01-23 14:40:29 +0100

    changed test problem

--- IJCAR/experiments/benchmarks.py
+++ IJCAR/experiments/benchmarks.py
@@ -18,7 +18,7 @@ class Test:
     offsets= [[1, 0]]
 
     occ= [[Int('occf1'), Int('occf2')]]     # variables for occurrences of funct. symb. (u, v in the paper)
-    argF= [[0]]                             # arguments of uninterpreted function symbols in F
-    F= f(argF[0][0])==0
+    argF= [[4]]                             # arguments of uninterpreted function symbols in F
+    F= f(argF[0][0])==7
     Q= Or(x<0, x>=1000, f(x+offsets[0][0])==f(x+offsets[0][1])+1)
     Qp= Or(x<0, x>=1000, occ[0][0]==occ[0][1]+1)
