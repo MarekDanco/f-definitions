d9e74f4bb503d74a1416b4887d4ec95365356721
Stefan Ratschan <stefan.ratschan@cs.cas.cz>
2026-01-26 15:05:48 +0100

    added benchmark

--- IJCAR/experiments/benchmarks.py
+++ IJCAR/experiments/benchmarks.py
@@ -18,11 +18,22 @@ class IncrConst:
     offsets= [[1, 0]]
 
     occ= [[Int('occf1'), Int('occf2')]]     # variables for occurrences of funct. symb. (u, v in the paper)
-    argF= [[0]]                             # arguments of uninterpreted function symbols in F
+    argF= [[c]]                             # arguments of uninterpreted function symbols in F
     F= f(argF[0][0])==c
     Q= Or(x<0, x>=1000, f(x+offsets[0][0])==f(x+offsets[0][1])+1)
     Qp= Or(x<0, x>=1000, occ[0][0]==occ[0][1]+1)
 
+class IncrConstArg:
+    x= Int('x')                                 # the quantified variable    
+    f= Function('f', IntSort(), IntSort())
+    c= Int('c')
+    offsets= [[c, 0]]
+
+    occ= [[Int('occf1'), Int('occf2')]]     # variables for occurrences of funct. symb. (u, v in the paper)
+    argF= [[c+1]]                             # arguments of uninterpreted function symbols in F
+    F= And(c>=0, f(argF[0][0])==c)
+    Q= Or(x<0, x>=1000, f(x+offsets[0][0])==f(x+offsets[0][1])+1)
+    Qp= Or(x<0, x>=1000, occ[0][0]==occ[0][1]+1)
     
 class Incr2Functions:
     x= Int('x')                                 # the quantified variable    
