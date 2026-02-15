4e7f5ca0d6315eee1fe5c8fd5ffcc6ed3f3c224c
marek <marekdanco.mail@gmail.com>
2026-02-15 23:05:48 +0100

    sygus

--- IJCAR/experiments/data/mk_data_frame.py
+++ IJCAR/experiments/data/mk_data_frame.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+# import numpy as np
 
 C_MAP = {
-    "bench10_1000_40000": 1,
-    "bench100_1000_40000": 2,
-    "bench1000_1000_40000": 3,
-    "bench10000_1000_40000": 4,
-    "bench100000_1000_40000": 5,
-    "bench_1000_40000": "✱",
+    "bench10_700_40000": 1,
+    "bench100_700_40000": 2,
+    "bench1000_700_40000": 3,
+    "bench10000_700_40000": 4,
+    "bench100000_700_40000": 5,
+    "bench_700_40000": "✱",
 }
 
 C_MAP_MIN = {
@@ -61,11 +61,16 @@ rows = []
 for folder, c in C_MAP.items():
     for solver in ["cvc5", "cvc5-sg", "z3"]:
         tab_file = base1 / folder / f"{solver}.tab"
+        if solver == "cvc5-sg":
+            solver = "cvc5-sygus"
         rows.extend(parse_tab(tab_file, solver, c))
 
 for folder, c in C_MAP_MIN.items():
-    tab_file = base2 / folder / "cvc5.tab"
-    rows.extend(parse_tab(tab_file, "cvc5-min", c))
+    for solver in ["cvc5", "cvc5-sg"]:
+        tab_file = base2 / folder / f"{solver}.tab"
+        if solver == "cvc5-sg":
+            solver = "cvc5-sygus"
+        rows.extend(parse_tab(tab_file, f"{solver}-min", c))
 
 
 
@@ -109,26 +114,26 @@ plt.tight_layout()
 plt.savefig("scaling_plot.png", dpi=300, bbox_inches="tight")
 plt.close()
 
-TIMEOUT = 1800
-fig, axes = plt.subplots(1, 2, figsize=(9, 3), sharey=True)
-cs = sorted(df[df["c"].isin([2, 3])]["c"].unique())
-for ax, c in zip(axes, cs):
-    sub = df[(df["c"] == c) & (df["solver"] == "cvc5")]
-    solved = sub[sub["status"] == "sat"]["time_s"]
-    times = np.sort(solved.values)
-    x = np.arange(1, len(times) + 1)
-    ax.plot(
-        x,
-        times,
-        marker="^",
-        markersize=4,
-        linewidth=1.5,
-    )
-    ax.set_title(f"c = {c}")
-    ax.set_xlabel("Number of problems solved")
-    ax.set_yscale("symlog", linthresh=0.1)
-    ax.set_ylim(0, TIMEOUT)
-axes[0].set_ylabel("Runtime (s)")
-plt.tight_layout()
-plt.savefig("cactus_plot.png", dpi=300)
-plt.close()
+# TIMEOUT = 1800
+# fig, axes = plt.subplots(1, 2, figsize=(9, 3), sharey=True)
+# cs = sorted(df[df["c"].isin([2, 3])]["c"].unique())
+# for ax, c in zip(axes, cs):
+#     sub = df[(df["c"] == c) & (df["solver"] == "cvc5")]
+#     solved = sub[sub["status"] == "sat"]["time_s"]
+#     times = np.sort(solved.values)
+#     x = np.arange(1, len(times) + 1)
+#     ax.plot(
+#         x,
+#         times,
+#         marker="^",
+#         markersize=4,
+#         linewidth=1.5,
+#     )
+#     ax.set_title(f"c = {c}")
+#     ax.set_xlabel("Number of problems solved")
+#     ax.set_yscale("symlog", linthresh=0.1)
+#     ax.set_ylim(0, TIMEOUT)
+# axes[0].set_ylabel("Runtime (s)")
+# plt.tight_layout()
+# plt.savefig("cactus_plot.png", dpi=300)
+# plt.close()
